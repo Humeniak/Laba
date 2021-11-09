447dd4d9805b5530b993cf0e4538d1decf6a179c
Humeniak <93980420+Humeniak@users.noreply.github.com>
2021-11-09 12:33:51 +0200

    Add files via upload

---
+++ LR1_1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+   <head>
+       <title>LR1</title>
+   </head>
+   <body>
+      <?php
+          $name= "Sviatoslav";
+          echo "Hello, ".$name."!";
+      ?>
+   </body>
+</html>
+
+
